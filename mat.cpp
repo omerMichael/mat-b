1b6d4e37df55726ad3d23849826741d283e4cdda
Omer Michael <74428768+omerMichael@users.noreply.github.com>
2022-03-15 15:08:03 +0200

    Update mat.cpp

--- mat.cpp
+++ mat.cpp
@@ -7,12 +7,12 @@ namespace ariel{
     string create_pref(int size, char s1, char s2){ 
 
         if (size==1){
-            string p = ""; 
+            string p = string(""); 
             p += s1;
             return p;
         }
 
-        string ans = "";
+        string ans = string(""); 
         int i = 0;
         char symbol = s1;
 
@@ -28,27 +28,52 @@ namespace ariel{
     }
 
     string create_post(int size, char s1, char s2){ 
-        if(size %2==1) {
+        if(size % 2 == 1) {
             return create_pref(size , s1 , s2);
-        } else {
-            //mone cfilot cod
-            return create_pref(size , s2 , s1);
         }
+        return create_pref(size , s2 , s1);
     }
+
+    string create_mid(int row, int column, int size, char s1, char s2) {
+        if (column == 1) {
+            return string("");
+        }
+
+        string sym = string(""); 
+        string ans = string(""); 
+        sym += (row % 2 == 0) ? s1 : s2;
+        
+        for (int j = 0; j < column - (2 * size); j++) {
+            ans.append(sym);
+        }
+
+        return ans;
+    }
+
 
     string mat (int column , int row , char s1 , char s2){
 
         /*
-9 7  cols gt rows
-@@@@@@@@@ 0 
+9 7  cols > rows
+@@@@@@@@@ 0
 @-------@ 1
 @-@@@@@-@ 2
-@-@---@-@ 3  pref = 4 == "rows/2~ + 1 
+@-@---@-@ 3  i == "rows/2
 @-@@@@@-@ 4  # reverese
 @-------@ 5
 @@@@@@@@@ 6
 
-7 9 cols lt rows
+7 7
+@@@@@@@
+@-----@
+@-@@@-@
+@-@--@-@
+@-@@@-@
+@-----@
+@@@@@@@
+
+
+7 9 cols <= rows
 @@@@@@@ 0
 @-----@ 1
 @-@@@-@ 2
@@ -61,46 +86,61 @@ namespace ariel{
 
         */
 
-        string MAT = "";
+    //    cout << "column: " << column << "\nrow: " << row << endl;
+
+        if(column %2 == 0 || column < 0 || row %2 == 0 || row < 0 ){
+            throw invalid_argument("Ilegal input");
+        }
+
+        string MAT = string(""); 
        
 
         int pref_size = 1;
-        //0 or 1
-        int framei = 0;
         int i = 0;
         string sym;
+        bool flag = false;
 
         while(i < row){
 
             MAT.append(create_pref(pref_size,s1,s2));
-
-            cout << pref_size << endl;
-            sym = "";
-            if((framei++) %2== 0){
-                sym += s1;
-            }else{
-                sym += s2;
+            if (column == 1) {
+                MAT.append("\n");
+                i++;
+                continue;
             }
-            for (int j = pref_size; j < column - (2 * pref_size); j++){
+            string mid = create_mid(i, column, pref_size, s1, s2);
+            MAT.append(mid);
 
-                MAT.append(sym);
+            if(pref_size == (column/2) && row >= column){
+
+                MAT.append(create_post(pref_size,s1,s2));
+                MAT.append("\n");
+                i++;
+                int t = 0;
+
+                while (t < row - 2*i) {
+
+                    MAT.append(create_pref(pref_size, s1, s2));
+                    MAT.append(create_post(pref_size+1, s1, s2));
+                    MAT.append("\n");
+                    t++;
+                }
+                i += t;
+                flag = true;
+                pref_size--;
+                continue;
+            }
+            
+            if (i >= row/2) {
+
+                flag = true;
             }
 
-            if((pref_size == row / 2) && row > column){
-
-                cout << "implement row gt col"<<endl;
-            }
-            else if (pref_size == (row/2 +1)){
-
-                cout << "implement"<<endl;
-            }
-
-
-
-            MAT.append(create_post(pref_size,s1,s2));
+            
+            MAT.append(create_post(pref_size, s1, s2));
             MAT.append("\n");
             i++;
-            pref_size++;
+            pref_size += (flag) ? -1 : 1;
         }
         
 
