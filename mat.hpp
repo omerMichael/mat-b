d23b10fdc506ab1fc5ae0e068021bf050e63f264
Omer Michael <74428768+omerMichael@users.noreply.github.com>
2022-03-15 15:08:26 +0200

    Update mat.hpp

--- mat.hpp
+++ mat.hpp
@@ -9,4 +9,5 @@ namespace ariel{
     string mat (int col , int row , char sym1 , char sym2);
     string create_pref(int size, char s1, char s2);
     string create_post(int size, char s1, char s2);
+    string create_mid(int row, int column, int prefix_size, char s1, char s2);
 }
